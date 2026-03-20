a465205fccfdd333d4a89255e733837e32768e25
Protein Born <paulopantaleaonetto@gmail.com>
2026-03-20 16:37:16 -0300

    loop op, debug removed

--- input.py
+++ input.py
@@ -7,33 +7,46 @@ write.line()
 palavra = palavras.sortear_palavra().upper()
 oculta = palavras.palavra_oculta(palavra)
 usadas = []
-print(palavra)
 vidas = 6
+sleep(2)
 while True:
     while True:
         for display in oculta:
             print(display, end=' ')
         print('!')
+        sleep(1)
         if '_' not in oculta:
             break
         print(f'{vidas} vidas restantes!')
         write.line()
         if vidas == 0:
+            for revelar in palavra:
+                print(revelar, end=' ')
+            write.line()
             break
+        sleep(1)
         palpite = str(input('Digite seu palpite: ')[0]).upper()
         write.line()
-        acerto = False
+        sleep(1)
+        acerto = 0
         for pos, letra in enumerate(palavra):
-            if palpite in letra and palpite not in usadas:
+            if palpite in usadas:
+                acerto = -1
+                break
+            elif palpite in letra:
                 oculta[pos] = palpite
-                acerto = True
-        if acerto is False:
+                acerto += 1
+        if acerto == 0:
             vidas -= 1
             print('Você errou! -1 vida.')
-        else:
+        elif acerto > 0:
             print('Você acertou o palpite!')
             usadas.append(palpite)
+        else:
+            print('Já tem essa letra!')
         write.line()
+        sleep(2)
+    sleep(2)
     write.line()
     if vidas > 0:
         write.title('VOCÊ VENCEU!')
@@ -44,6 +57,7 @@ while True:
     while decidir not in ['S', 'N']:
         decidir = str(input('Deseja continuar[S/N]?')[0]).upper()
     if decidir == 'N':
+        sleep(2)
         write.line()
         write.title('OBRIGADO POR JOGAR!')
         write.line()
@@ -53,8 +67,8 @@ while True:
         palavra = palavras.sortear_palavra().upper()
         oculta = palavras.palavra_oculta(palavra)
         usadas = []
+        sleep(2)
         write.line()
         write.title('JOGO DA FORCA.')
         write.line()
-        print(palavra)
         continue
--- palavras.py
+++ palavras.py
@@ -1,14 +1,11 @@
 from random import choice
-palavras = ['abacaxi',
-'melancia',
-'coragem',
-'borboleta',
-'pular',
-'esconder',
-'laranja',
-'curioso',
-'caminhar',
-'framboesa']
+palavras = [
+    'abacaxi', 'melancia', 'morango', 'uva', 'manga',
+    'laranja', 'limao', 'pera', 'maca', 'banana',
+    'framboesa', 'acerola', 'goiaba', 'maracuja', 'caju',
+    'abacate', 'kiwi', 'ameixa', 'cereja', 'figo',
+    'pitanga', 'carambola', 'graviola', 'jabuticaba', 'coco'
+]
 def sortear_palavra():
     return choice(palavras)
 def palavra_oculta(palavra):
